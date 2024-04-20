bea07cbbd807e10351ca0abb1d28ce37a62e32db
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-04-20 16:32:32 +0200

    Added constants

---
+++ News-MVVM/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  News-MVVM
+//
+//  Created by Noor El-Din Walid on 20/04/2024.
+//
+
+import Foundation
+
+enum AppConstants {
+    static let baseURL = "https://saurav.tech/NewsAPI"
+}
