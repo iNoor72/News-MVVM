de6d3054c8de6981082a9b1478ad1b7f32578ff7
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-05-12 21:17:09 +0300

    Revert "Combine"
    This reverts commit 10a6848a1d8c7cf09e33f63f316c20250dc3974e.

--- News-MVVM/Scenes/NewsListView/NewsListViewController.swift
+++ News-MVVM/Scenes/NewsListView/NewsListViewController.swift
@@ -6,45 +6,28 @@
 //
 
 import UIKit
-import Combine
 
 class NewsListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     private let cellIdentifier = "Unique-id"
     
     private let viewModel = NewsListViewModel()
-    private var bagCombine = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupTableView()
-        setupReactiveTableView()
-//        viewModel.completionClosure = {[weak self] in
-//            self?.tableView.reloadData()
-//        }
-//        
-//        viewModel.errorClosure = {[weak self] error in
-//            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//            label.text = "ERROR: \(String(describing: error))"
-//            
-//            self?.view.addSubview(label)
-//        }
-//        
-        //You don't need to call this here, call it in the ViewModel init.
-//        viewModel.fetchNews()
-    }
-    
-    private func setupReactiveTableView() {
-        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        viewModel.reactiveArticlesCombine.sink { error in
-            print(error)
-        } receiveValue: { articles in
-            self.tableView.reloadData()
+        setupTableView()
+        viewModel.completionClosure = {[weak self] in
+            self?.tableView.reloadData()
         }
-        .store(in: &bagCombine)
+        
+        viewModel.errorClosure = {[weak self] error in
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+            label.text = "ERROR: \(String(describing: error))"
+            
+            self?.view.addSubview(label)
+        }
+        
+        viewModel.fetchNews()
     }
 
     private func setupTableView() {
