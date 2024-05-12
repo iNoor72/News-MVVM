30978a85aa8232f05d50b3837a651044b3c17020
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-05-12 21:15:55 +0300

    Combine

--- News-MVVM/Scenes/NewsListView/NewsListViewController.swift
+++ News-MVVM/Scenes/NewsListView/NewsListViewController.swift
@@ -6,28 +6,45 @@
 //
 
 import UIKit
+import Combine
 
 class NewsListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     private let cellIdentifier = "Unique-id"
     
     private let viewModel = NewsListViewModel()
+    private var bagCombine = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTableView()
-        viewModel.completionClosure = {[weak self] in
-            self?.tableView.reloadData()
-        }
+//        setupTableView()
+        setupReactiveTableView()
+//        viewModel.completionClosure = {[weak self] in
+//            self?.tableView.reloadData()
+//        }
+//        
+//        viewModel.errorClosure = {[weak self] error in
+//            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//            label.text = "ERROR: \(String(describing: error))"
+//            
+//            self?.view.addSubview(label)
+//        }
+//        
+        //You don't need to call this here, call it in the ViewModel init.
+//        viewModel.fetchNews()
+    }
+    
+    private func setupReactiveTableView() {
+        tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        viewModel.errorClosure = {[weak self] error in
-            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-            label.text = "ERROR: \(String(describing: error))"
-            
-            self?.view.addSubview(label)
+        viewModel.reactiveArticlesCombine.sink { error in
+            print(error)
+        } receiveValue: { articles in
+            self.tableView.reloadData()
         }
-        
-        viewModel.fetchNews()
+        .store(in: &bagCombine)
     }
 
     private func setupTableView() {
