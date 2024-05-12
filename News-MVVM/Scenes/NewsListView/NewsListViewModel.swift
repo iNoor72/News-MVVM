10a6848a1d8c7cf09e33f63f316c20250dc3974e
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-05-12 21:15:55 +0300

    Combine

--- News-MVVM/Scenes/NewsListView/NewsListViewModel.swift
+++ News-MVVM/Scenes/NewsListView/NewsListViewModel.swift
@@ -6,17 +6,21 @@
 //
 
 import Foundation
+import Combine
 
 //final is related to Static & Dynamic dispatch
 final class NewsListViewModel {
     private let network: AlamofireNetworkService
     private var articles: [Article] = []
     
+    var reactiveArticlesCombine = PassthroughSubject<[Article], NSError>()
+    
     var completionClosure: (() -> ())?
     var errorClosure: ((String?) -> ())?
     
     init(network: AlamofireNetworkService = AlamofireNetworkService()) {
         self.network = network
+        fetchNews()
     }
     
     func fetchNews() {
@@ -31,6 +35,7 @@ final class NewsListViewModel {
                 
             case .success(let newsResponse):
                 self.articles = newsResponse.articles ?? []
+                self.reactiveArticlesCombine.send(newsResponse.articles ?? [])
                 DispatchQueue.main.async {
                     if let completionClosure = self.completionClosure {
                         completionClosure()
